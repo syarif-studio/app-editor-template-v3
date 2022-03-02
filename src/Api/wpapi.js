f3ca5972c61d2ce7371daf63284ced247abad00c
syarif-studio <syarifuddin.studio@gmail.com>
2022-03-02 14:50:16 +0800

    clean wpapi.js

--- src/Api/wpapi.js
+++ src/Api/wpapi.js
@@ -3,8 +3,4 @@ import { config } from "../../config";
 
 export const wpapi = new WPAPI({
   endpoint: config.baseUrl + "/wp-json",
-  username: "admin",
-  password: "123456",
 });
-
-//wpapi.product = wpapi.registerRoute( 'wp/v2', '/product/(?P<id>\\d+)' );
