c29a31aa5306913091e6aecc0def753fd85fcef2
syarif-studio <syarifuddin.studio@gmail.com>
2022-11-16 20:29:57 +0800

    Fix bug use get product

--- src/Hook/Woocommerce/Product.js
+++ src/Hook/Woocommerce/Product.js
@@ -4,47 +4,45 @@ import { PostTypeProvider } from "../PostTypeContext";
 import { wooapi } from "../../Api";
 import { useItem } from "../PostContent";
 
-async function fetchProduct(query) {
-  if (query?.related) {
-    delete query.related;
-    query = { ...query, category: product.categories?.[0]?.id };
-  }
-
-  //remove query with false value
-  Object.keys(query).forEach((key) => {
-    if (query[key] === false) {
-      delete query[key];
-    }
-  });
-  const response = await wooapi.get("products", query);
-  return response?.data;
-}
-
-async function fetchData(json) {
-  const param = JSON.parse(json);
-  let { postType, ...query } = param;
-
-  let products = [];
-  if (query?.bestSeller) {
-    let response = await wooapi.get("reports/top_sellers", { period: "year" });
-    let { bestSeller, ...bestSellerQuery } = query;
-    if (response?.data?.length) {
-      const include = response?.data?.map((item) => item.product_id).join();
-      bestSellerQuery = { ...bestSellerQuery, include };
-    }
-    products = await fetchProduct(bestSellerQuery);
-  } else {
-    products = await fetchProduct(query);
-  }
-
-  return products;
-}
-
 function useGetProductData(query = {}) {
   const product = useItem();
-  if (query?.related) {
-    delete query.related;
-    query = { ...query, category: product.categories?.[0]?.id };
+
+  async function fetchProduct(query) {
+    if (query?.related) {
+      delete query.related;
+      query = { ...query, category: product.categories?.[0]?.id };
+    }
+
+    //remove query with false value
+    Object.keys(query).forEach((key) => {
+      if (query[key] === false) {
+        delete query[key];
+      }
+    });
+    const response = await wooapi.get("products", query);
+    return response?.data;
+  }
+
+  async function fetchData(json) {
+    const param = JSON.parse(json);
+    let { postType, ...query } = param;
+
+    let products = [];
+    if (query?.bestSeller) {
+      let response = await wooapi.get("reports/top_sellers", {
+        period: "year",
+      });
+      let { bestSeller, ...bestSellerQuery } = query;
+      if (response?.data?.length) {
+        const include = response?.data?.map((item) => item.product_id).join();
+        bestSellerQuery = { ...bestSellerQuery, include };
+      }
+      products = await fetchProduct(bestSellerQuery);
+    } else {
+      products = await fetchProduct(query);
+    }
+
+    return products;
   }
 
   const json = JSON.stringify({ postType: "product", ...query });
